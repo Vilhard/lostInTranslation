e850d0764858df5ae123702896f67eada1f17aca
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-09-20 23:54:15 +0300

    Added basic folder structure & components

---
+++ src/api/TranslationsAPI.js
@@ -0,0 +1,6 @@
+const apiURL = 'https://noroff-assignment-api-lit.herokuapp.com/'
+const apiKey = 'ByvuHqRoCVXC9G9Z06xa3ec9rDXYgZyJZRDXJ9k3arjVxy2AuUXX6c34Z2dgnlx2'
+
+export const TranslationsAPI = {
+
+}
