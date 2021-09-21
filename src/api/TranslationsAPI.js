107159b2d302d5e942e43e4401c69d7bbfe89b6d
ville <ville.hotakainen@gmail.com>
2021-09-21 17:51:22 +0300

    Feat: user fetch and creation functionality added

--- src/api/TranslationsAPI.js
+++ src/api/TranslationsAPI.js
@@ -1,6 +1,84 @@
-const apiURL = 'https://noroff-assignment-api-lit.herokuapp.com/'
-const apiKey = 'ByvuHqRoCVXC9G9Z06xa3ec9rDXYgZyJZRDXJ9k3arjVxy2AuUXX6c34Z2dgnlx2'
+import axios from "axios";
 
-export const TranslationsAPI = {
+const apiURL = 'https://noroff-assignment-api-lit.herokuapp.com'
+const apiKey = "ByvuHqRoCVXC9G9Z06xa3ec9rDXYgZyJZRDXJ9k3arjVxy2AuUXX6c34Z2dgnlx2";
 
-}
+const TranslationsAPI = {
+    // async fetchUser(username) {
+    //     try {
+    //       const result = await fetch(`${apiURL}/translations?username=${username}`)
+	// 		.then((response) => response.json())
+	// 		.then((results) => {
+	// 			// results will be an array of users that match the username of victor.
+	// 			console.log("Users fetched: " + results[0])
+    //             const found = results.find(user => user.username === username)
+                
+	// 		})
+    //     } catch(error) {
+    //         console.log(error.message)
+    //     }
+		
+	// },
+     async getUsername(username) {
+       return await axios.get(`${apiURL}/translations?username=${username}`).then(response => 
+            response.data
+        )
+    },
+	async fetchTranslations(username) {
+		fetch(`${apiURL}/translations?username=${username}`)
+			.then((response) => response.json())
+			.then((results) => {
+				// results will be an array of users that match the username of argument.
+				console.log("fetchTranslations: " + results);
+			})
+			.catch((error) => {});
+	},
+	async setNewUser(username) {
+		fetch(`${apiURL}/translations`, {
+			method: "POST",
+			headers: {
+				"X-API-Key": apiKey,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: username,
+				translations: [],
+			}),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Could not create new user");
+				}
+				return response.json();
+			})
+			.then((newUser) => {
+				// newUser is the new user with an id
+				console.log("New User: " + newUser);
+			})
+			.catch((error) => {});
+	},
+	async updateTranslations(userId) {
+		fetch(`${apiURL}/translations/${userId}`, {
+			method: "PATCH", // NB: Set method to PATCH
+			headers: {
+				"X-API-Key": apiKey,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				// Provide new translations to add to user with id 1
+				translations: ["easy", "i love javascript"],
+			}),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Could not update translations history");
+				}
+				return response.json();
+			})
+			.then((updatedUser) => {
+				// updatedUser is the user with the Patched data
+			})
+			.catch((error) => {});
+	},
+};
+export default TranslationsAPI
--- src/components/Start.js
+++ src/components/Start.js
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import StartHeader from "./StartHeader";
+import TranslationsAPI from "../api/TranslationsAPI";
+import './StartStyles.css'
+
 const Start = () => {
-    return (  
-        <h1>Start page</h1>
+
+    const [user, setUser] = useState({
+        username: '',
+        translations: []
+    })
+    const onInputChange = e => {
+        setUser({
+            ...user,
+            [e.target.id]: e.target.value
+        })
+    }
+    const onLoginSubmit = async e => {
+        e.preventDefault()
+        const fetchUser = await TranslationsAPI.getUsername(user.username)
+       fetchUser.length === 0 ? TranslationsAPI.setNewUser(user.username) : console.log('User exists')
+    }
+
+    return (
+        <div>
+            <StartHeader />
+            <form className="form-container">
+                <div>
+                   <input id="username" type="text" placeholder="What's your name?" onChange={onInputChange} className="form-input"/> 
+                   <button type="submit" onClick={onLoginSubmit}>GO</button>
+                </div>
+            </form>
+        </div>
     );
 }
 
