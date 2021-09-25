cc87a9815a2b2743df91dfd0e227bb6397f5cb0b
Vilhard <ville.hotakainen@gmail.com>
2021-09-25 22:31:31 +0300

    Refactor: removed profile avatar from navbar and login text from startup page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,48 @@
-import styles from './Navbar.module.css'
-import profilePic from '../assets/profile-blank.png';
+import styles from "./Navbar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { useHistory } from "react-router-dom";
 
 const Navbar = () => {
-    const username = localStorage.getItem('username')
-    const history = useHistory();
+  const username = localStorage.getItem("username");
+  const history = useHistory();
 
-    function logoutUser() {
-		localStorage.removeItem("username");
-		localStorage.removeItem("id");
-        history.push("/");
-	}
-    function goToFrontPage(){
-        history.push("/")
-    }
-    function goToProfilePage(){
-        history.push("/profile")
-    }
+  function logoutUser() {
+    localStorage.removeItem("username");
+    localStorage.removeItem("id");
+    history.push("/");
+  }
+  function goToProfilePage() {
+    history.push("/profile");
+  }
 
-    const userProfile = () => {
-        if(username === null){
-            return <div className={styles.NavbarUser}>
-                <span className={styles.NavbarUserLogin} onClick={goToFrontPage}>Log in</span>
-            </div>
-        }else{
-            return <div className={styles.NavbarUser}>
-                <div onClick={goToProfilePage}>
-                    <span className={styles.NavbarUserName}>{username}</span>  
-                    <img className={styles.NavbarUserPic} src={profilePic} alt="Profile_picture"/>
-                </div>
-                <button onClick={logoutUser}>Log out</button>
-                    
-                </div>
-        }
+  const userProfile = () => {
+    if (username !== null) {
+      return (
+        <div className={styles.NavbarUser}>
+          <div onClick={goToProfilePage}>
+            <span className={styles.NavbarUserName}>{username}</span>
+          </div>
+          <button styles={styles.NavBarButton} onClick={logoutUser}>
+            <FontAwesomeIcon icon={faSignOutAlt} size="1x" />
+          </button>
+        </div>
+      );
     }
-    return (
-        <>
-            <nav className={styles.Navbar}>
-                <ul className={styles.NavbarMenu}>
-                    <li className={styles.NavbarHeadingText}><a className={styles.NavbarUserLogin} href="/">Lost in Translation</a></li>
-                    <li>{userProfile()}</li>
-                </ul>
-
-            </nav>
-        </>
-    )
-}
+  };
+  return (
+    <>
+      <nav className={styles.Navbar}>
+        <ul className={styles.NavbarMenu}>
+          <li className={styles.NavbarHeadingText}>
+            <a className={styles.NavbarUserLogin} href="/">
+              Lost in Translation
+            </a>
+          </li>
+          <li>{userProfile()}</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 export default Navbar;
